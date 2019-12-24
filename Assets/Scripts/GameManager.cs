e6894738457b4fca22e005f8df6ceb09e73bac34
Xun <galaxymacos@gmail.com>
2019-12-24 16:37:03 -0500

    Basic Project?
    This is the base project I created. It is just for fun.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player1;
+    public Player player2;
+
+    public GameBoard gameBoard;
+    
+    [HideInInspector]
+    public GameState gameState;
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        gameState = GameState.Preparing;
+        // Preparation
+
+        gameState = GameState.Player1Turn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (gameState)
+        {
+            case GameState.Player1Turn:
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    player1.MoveRandomly();
+                    if (!CheckIfAnyoneWin())
+                    {
+                        gameState = GameState.Player2Turn;
+                    }
+                }
+
+                break;
+            case GameState.Player2Turn:
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    player2.MoveRandomly();
+                    if (!CheckIfAnyoneWin())
+                    {
+                        gameState = GameState.Player1Turn;
+                    }
+                }
+
+                break;
+        }
+        
+    }
+
+    public bool CheckIfAnyoneWin()
+    {
+        if (player1.ReachEnd() || player2.ReachEnd())
+        {
+            gameState = GameState.GameOver;
+            if (player1.ReachEnd())
+            {
+                print("Player 1 won");
+            }
+            if(player2.ReachEnd())
+            {
+                print("Player 2 won");
+            }
+            return true;
+            
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+    
+    
+}
+
+public enum GameState{Preparing, Player1Turn, Player2Turn, GameOver}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private GameBoard _gameBoard;
+
+    public int PositionIndex { get; set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameBoard = GameManager.Instance.gameBoard;
+        PositionIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveTo(int newPositionIndex)
+    {
+        PositionIndex = newPositionIndex;
+        transform.localPosition = _gameBoard.wayPoints[newPositionIndex].localPosition;
+    }
+
+    public void MoveRandomly()
+    {
+        int newPositionIndex = Random.Range(1, 7) + PositionIndex;
+        StartCoroutine(MoveToDuringTime(newPositionIndex));
+    }
+
+    private IEnumerator MoveToDuringTime(int newPositionIndex)
+    {
+        while (PositionIndex < newPositionIndex)
+        {
+            MoveTo(PositionIndex+1);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public bool ReachEnd()
+    {
+        return PositionIndex == _gameBoard.wayPoints.Length - 1;
+    }
+}
