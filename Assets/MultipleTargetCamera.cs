8a3794906adc20fb21a0dd38c00b8c55dd04ee7a
Xun <galaxymacos@gmail.com>
2019-12-25 12:46:10 -0500

    very very very very simple camera added

---
+++ Assets/MultipleTargetCamera.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MultipleTargetCamera : MonoBehaviour
+{
+    public List<Transform> targets;
+
+    public Vector3 offset;
+
+    public float smoothTime = .5f;
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+    
+    public Vector3 velocity;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        if (targets.Count == 0)
+        {
+            return;
+        }
+
+        Move();
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / 7f);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    private float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
+    }
+
+    private void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint+offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    private Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        foreach (var t in targets)
+        {
+            bounds.Encapsulate(t.position);
+        }
+        return bounds.center;
+    }
+}
