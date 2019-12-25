577d54cf9b81ec44da5a8d2ba6d6c8799a65bc86
Xun <galaxymacos@gmail.com>
2019-12-24 21:51:32 -0500

    Add a very very very simple audio manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] soundInfos;
+
+    public static AudioManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+            
+        
+        foreach (var s in soundInfos)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.audioClip;
+            
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("Bgm");
+    }
+
+    public void Play(string soundNameToPlay)
+    {
+        Sound s = Array.Find(soundInfos, sound => sound.audioName == soundNameToPlay);
+        if (s == null) // Can't find the sound
+        {
+            Debug.LogWarning("Sound: "+soundNameToPlay+" not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundInfo", menuName = "Sound/SoundInfo")]
+public class Sound : ScriptableObject
+{
+    public string audioName;
+    public AudioClip audioClip;
+    [Range(0f,1f)]
+    public float volume;
+    [Range(.1f,3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+    
+}
