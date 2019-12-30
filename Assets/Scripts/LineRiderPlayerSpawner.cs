d127277db59c89f8ef42e135c5072b105791c3f4
Xun <galaxymacos@gmail.com>
2019-12-29 20:00:16 -0500

    Add Mini Game Line Rider

---
+++ Assets/Scripts/LineRiderPlayerSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRiderPlayerSpawner : MonoBehaviour
+{
+    public GameObject player;
+
+    public static LineRiderPlayerSpawner Instance;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        Spawn(0);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn(float seconds)
+    {
+        StartCoroutine(SpawnAfterSeconds(seconds));
+    }
+
+    IEnumerator SpawnAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        var playerGameObject = Instantiate(player, transform.position, Quaternion.identity);
+        LineRiderGameManager.Instance.player = playerGameObject.GetComponent<LineRiderPlayer>();
+    }
+}
---
+++ Assets/Scripts/LineRiderUiController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LineRiderUiController : MonoBehaviour
+{
+    // Reference
+    public TextMeshProUGUI missionText;
+    public TextMeshProUGUI subMissionText;
+    public TextMeshProUGUI inkLeftText;
+    public TextMeshProUGUI attemptLeftText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inkLeftText.text = $"Ink left: {LineRiderGameManager.Instance.inkLeft}";
+        attemptLeftText.text = $"Attempt left: {LineRiderGameManager.Instance.attemptTimes}";
+    }
+
+    public void HideTipText()
+    {
+        missionText.gameObject.SetActive(false);
+        subMissionText.gameObject.SetActive(false);
+    }
+}
